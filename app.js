7d9bcfef3d63bfb3ad978444f1eeab04953dec0c
Lin Yiquan <lily@moe.lu>
2016-06-18 13:42:17 +0900

    1
    1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const app = require('express')();
-const crypto = require('crypto'), sha1sum = crypto.createHash('sha1');
+var app = require('express')();
+var crypto = require('crypto'), sha1sum = crypto.createHash('sha1');
 
-const token = "1234567890"
+var token = "1234567890"
 
 app.get("/",(req,res,next)=>{
-	let arr [req.query.timestamp,req.query.nonce,token];
+	var arr [req.query.timestamp,req.query.nonce,token];
 	sha1sum.update(array.sort().join(""));
-	let sign = sha1sum.digest('hex')
+	var sign = sha1sum.digest('hex')
 	console.log(sign);
 	if (sign == req.query.signature) {
 		res.send(req.query.echostr);
