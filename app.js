bc72e9006347f135ce083fce82ac9fb89c852911
Lin Yiquan <lily@moe.lu>
2016-06-18 13:44:16 +0900

    1
    1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var token = "1234567890"
 
 app.get("/",(req,res,next)=>{
 	var arr = [req.query.timestamp,req.query.nonce,token];
-	sha1sum.update(array.sort().join(""));
+	sha1sum.update(arr.sort().join(""));
 	var sign = sha1sum.digest('hex')
 	console.log(sign);
 	if (sign == req.query.signature) {
