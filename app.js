3d57ad505c5280e9306c4b38718e8e8a21543b14
Lin Yiquan <lily@moe.lu>
2016-06-18 14:21:30 +0900

    1
    1

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var app = require('express')();
+var WechatAPI = require('wechat-api');
 var crypto = require('crypto'), sha1sum = crypto.createHash('sha1');
-
-var token = "1234567890"
+var config = require('./config.json');
+var api = new WechatAPI(config.app_id, config.appsecret);
 
 app.get("/",(req,res,next)=>{
-	var arr = [req.query.timestamp,req.query.nonce,token];
+	var arr = [req.query.timestamp,req.query.nonce,config.token];
 	sha1sum.update(arr.sort().join(""));
 	var sign = sha1sum.digest('hex')
 	console.log(sign);
 	if (sign == req.query.signature) {
 		res.send(req.query.echostr);
 	}
+});
+
+api.createMenu(config.menu,(err,res)=>{
+	
 });
 
 app.listen(80);
