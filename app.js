9f9da18f1550c59a54a8f9386037b425680c54b4
Lin Yiquan <lily@moe.lu>
2016-06-18 13:23:06 +0900

    1
    1

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const app = require('express')();
 
 app.get("/",(req,res,next)=>{
 	console.log(req.query.signature);
+	console.log(req.query.echostr);
+	res.send(req.query.echostr);
 });
 
 app.listen(80);
