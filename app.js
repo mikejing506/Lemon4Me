1b9b37e12e2af88f5fa5db18dae035e42fd302a4
Lin Yiquan <lily@moe.lu>
2016-06-20 09:38:44 +0900

    1
    1

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var WechatAPI = require('wechat-api');
 var crypto = require('crypto'), sha1sum = crypto.createHash('sha1');
 var config = require('./config.json');
 var api = new WechatAPI(config.app_id, config.appsecret);
+var fs = require('fs');
+
+// WeChat API
 
 app.get("/",(req,res,next)=>{
 	var arr = [req.query.timestamp,req.query.nonce,config.token];
@@ -15,7 +18,31 @@ app.get("/",(req,res,next)=>{
 });
 
 api.createMenu(config.menu,(err,res)=>{
-	
+
 });
+
+// WebServer
+
+app.engine('rj',(page,callback)=>{
+  fs.readFile('views/index.html', function (err, content) {
+    if (err) return callback(new Error(err));
+    // this is an extremely simple template engine
+    var rendered = content.toString().replace('$js$', ''+ page +'.js');
+    return callback(null, rendered);
+  });
+});
+app.set('views', './views');
+app.set('view engine','rj');
+
+app.use('/public', express.static('public'));
+
+app.route("/shiye")
+	.get((req,res,next)=>{
+		res.render('shiye');
+	}).post((req,res,next)=>{
+
+	});
+
+app.route('/me').get().post();
 
 app.listen(80);
