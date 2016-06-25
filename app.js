845899fc03eb4a235a8ed395f0fb8ee5a4264c5e
Lin Yiquan <lily@moe.lu>
2016-06-25 23:23:04 +0900

    1
    1

--- app.js
+++ app.js
@@ -1,48 +1,56 @@
-var app = require('express')();
-var WechatAPI = require('wechat-api');
-var crypto = require('crypto'), sha1sum = crypto.createHash('sha1');
+var express = require('express');
+var app = express();
+// var WechatAPI = require('wechat-api');
+var path = require('path');
+// var crypto = require('crypto'), sha1sum = crypto.createHash('sha1');
 var config = require('./config.json');
-var api = new WechatAPI(config.app_id, config.appsecret);
+var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+var connection = mysql.createConnection(config.database);
+
+// var api = new WechatAPI(config.app_id, config.appsecret);
 var fs = require('fs');
 
 // WeChat API
 
-app.get("/",(req,res,next)=>{
-	var arr = [req.query.timestamp,req.query.nonce,config.token];
-	sha1sum.update(arr.sort().join(""));
-	var sign = sha1sum.digest('hex')
-	console.log(sign);
-	if (sign == req.query.signature) {
-		res.send(req.query.echostr);
-	}
-});
+// app.get("/",(req,res,next)=>{
+// 	var arr = [req.query.timestamp,req.query.nonce,config.token];
+// 	sha1sum.update(arr.sort().join(""));
+// 	var sign = sha1sum.digest('hex')
+// 	console.log(sign);
+// 	if (sign == req.query.signature) {
+// 		res.send(req.query.echostr);
+// 	}
+// });
 
-api.createMenu(config.menu,(err,res)=>{
+// api.createMenu(config.menu,(err,res)=>{
 
-});
+// });
 
 // WebServer
 
-app.engine('rj',(page,callback)=>{
-  fs.readFile('views/index.html', function (err, content) {
-    if (err) return callback(new Error(err));
-    // this is an extremely simple template engine
-    var rendered = content.toString().replace('$js$', ''+ page +'.js');
-    return callback(null, rendered);
-  });
-});
-app.set('views', './views');
-app.set('view engine','rj');
+// connection.connect();
 
-app.use('/public', express.static('public'));
+app.use(bodyParser());
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/assets', express.static('public/assets'));
 
 app.route("/shiye")
 	.get((req,res,next)=>{
 		res.render('shiye');
-	}).post((req,res,next)=>{
-
 	});
 
-app.route('/me').get().post();
+app.post("/shiye/last",(req,res,next)=>{
+	
+});
 
-app.listen(80);
+app.route('/me')
+	.get((req,res,next)=>{
+		res.render('shiye');
+	}).post();
+
+app.listen(8080);
